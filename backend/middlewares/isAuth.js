a237b1129072131a929f98d66af4a5916a516927
Prashant Kumar <prashant133191@gmail.com>
2025-12-26 07:29:19 +0530

    Now is Correct

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -1,9 +1,10 @@
+// This varifies that which token is passed by us is same is from stored token in browser these all varifification performed by JWT
 import jwt from 'jsonwebtoken'
 
 const isAuth = async(req, res, next)=>{
 
     try {
-        // get out the only token value from the cookie righther then all cookie
+        // get out the only token value from the cookie righther then all cookie so we write this in the {}
         let {token} = req.cookies
         if(!token){
             return res.status(400).json({message:"user doesn't token"})
@@ -12,7 +13,7 @@ const isAuth = async(req, res, next)=>{
         let verifyToken = await jwt.verify(token, process.env.JWT_SECRET)
         if(!verifyToken){
             return res.status(400).json({message:"user doesn't have valid token"})
-        }
+        } 
         
         req.userId = verifyToken.userId
         next()
